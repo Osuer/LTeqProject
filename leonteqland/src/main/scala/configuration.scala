9717631867bae6a3ce1c67261d997af7dbe9ca8c
Chane Bornman <chane.bornman@gmail.com>
2022-11-12 02:10:24 +0200

    Added comments to the configuration class

--- leonteqland/src/main/scala/configuration.scala
+++ leonteqland/src/main/scala/configuration.scala
@@ -1,18 +1,21 @@
 import scala.collection.mutable.ListBuffer
 object configuration {
-  def buildDirectedList(map: String): DirectedList ={
+  def buildDirectedList(inputString: String): DirectedList ={
     val charList: ListBuffer[Char] = new ListBuffer[Char]()
     var sortedList: List[Char] = List[Char]()
-    for (x <- 0 until map.length){
-      if (map.charAt(x).toUpper.toInt >= 65 && map.charAt(x).toUpper.toInt <= 90) {
-        charList.addOne(map.charAt(x).toUpper)
+    //For each character in the string, force it to uppercase and check if it's a letter by checking the ASCII values.
+    for (x <- 0 until inputString.length){
+      if (inputString.charAt(x).toUpper.toInt >= 65 && inputString.charAt(x).toUpper.toInt <= 90) {
+        charList.addOne(inputString.charAt(x).toUpper)
       }
     }
+    //Remove all duplicates found in the string and sort them.
     sortedList = charList.toList.distinct.sorted
-
+    //Create a new directed graph data structure
     val directedList: DirectedList = new DirectedList(sortedList)
-
-    val edgeArray: Array[String] = map.split(",")
+    //Split the input string by "," to get all the edge nodes for each node
+    val edgeArray: Array[String] = inputString.split(",")
+    //Add all the edge nodes to the data structure
     for (i <- edgeArray.indices){
       directedList.addEdge(edgeArray(i).trim().charAt(0), edgeArray(i).trim().charAt(1), edgeArray(i).trim().charAt(2) - 48)
     }
